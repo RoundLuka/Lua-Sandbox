cb4a4958c6c979547787388c7414c61579368b5a
RoundLuka <lukagurgenidze282007@gmail.com>
2024-11-14 23:54:51 +0400

    commit

--- Day 01/script.lua
+++ Day 01/script.lua
@@ -1,1 +1,19 @@
-local name = "luka"
+local kata = {}
+
+function kata.noSpace(str)
+  local res = ""
+  for i = 0, str.length do
+    if str[i] ~= " " then
+      res = res + str[i]
+    end
+   end
+    return res
+end
+
+return kata
+
+function ()
+    for index, value in ipairs(t) do
+        
+    end
+end
