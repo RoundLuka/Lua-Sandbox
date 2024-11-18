cbdc5e6021c83239c92382c1c130b80755134ed2
RoundLuka <lukagurgenidze282007@gmail.com>
2024-11-18 23:50:21 +0400

    commit

---
+++ Day 03/app.lua
@@ -0,0 +1,21 @@
+-- local array = {"Giorgi", "Luka", 3}
+
+-- local dict = {
+--     ["name1"] = "Giorgi",
+--     ["name2"] = "Luka",
+--     ["number1"] = 3
+-- }
+
+-- print(dict["name1"])
+-- print(dict["name2"])
+-- print(dict["number1"])
+
+-- for key, value in pairs(dict) do
+--     print(key, value)
+-- end
+
+if true then
+    print("Hello world")
+else 
+    print("Goodbye")
+end
