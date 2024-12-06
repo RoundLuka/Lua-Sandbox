a16279d461bba4db4d83500a98719c44027f8da6
RoundLuka <lukagurgenidze282007@gmail.com>
2024-12-06 23:36:44 +0400

    commit

--- Day 05/app.lua
+++ Day 05/app.lua
@@ -1,1 +1,43 @@
-123
+
+-- This is a comment
+-- Variables
+
+--[[
+this is
+multi line
+comment
+]]
+
+
+-- Output, printing
+
+-- print("Hello, Lua")
+
+-- -- Declaring variables
+-- local name = "Luka"
+
+-- -- while number != guess do
+    
+-- -- end
+
+-- -- Input
+-- local feedback = io.read();
+
+local secretNumber = 60
+local attempts = 0
+
+while true do
+    print("Please guess number (1-100) \n")
+    local number = tonumber(io.read());
+
+    if number == secretNumber then
+        break
+    elseif number > secretNumber then
+        print("Your guess is too high")
+    elseif  number < secretNumber then
+        print("Your number is too low")
+    end
+    attempts = attempts + 1
+end
+
+print("You took attempts " .. tostring(attempts) .. " to guess the number")
