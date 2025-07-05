c0d3753a286af0e71f585bbf1aa11bd85c8b3472
RoundLuka <lukagurgenidze282007@gmail.com>
2025-07-05 23:57:40 +0400

    commit

--- Day 10/main.lua
+++ Day 10/main.lua
@@ -1,1 +1,11 @@
-print()
+--2) შექმენით leaderstats, რომელშიც მოთამაშეს ექნება "ქულები" (Points) 0-დან დაწყებული
+
+-- game.Players.PlayerAdded:Connect(function(plr)
+-- 	local leaderstats = Instance.new("Folder", plr)
+-- 	leaderstats.Name = "leaderstats"
+	
+-- 	local points = Instance.new("IntValue", leaderstats)
+-- 	points.Name = "Points"
+-- 	points.Value = 0
+-- end)
+
